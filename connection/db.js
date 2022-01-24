04abcc165787e8e1b25fafff37cbcabadeb9a32f
Kevhagi <94234971+Kevhagi@users.noreply.github.com>
2022-01-24 16:36:17 +0700

    Tambah ssl di db.js

--- connection/db.js
+++ connection/db.js
@@ -5,7 +5,8 @@ const dbPool = new Pool({
     database : 'd51cch6db1f81f', //'personal_web',
     port : 5432,
     user :  'tirtuvwurrnxfb', //'postgres',
-    password : '24be50d5b539e88cfd2618b0a6156e885ed479b0e658c3628d031a0b70e27c13' //'admin'
+    password : '24be50d5b539e88cfd2618b0a6156e885ed479b0e658c3628d031a0b70e27c13', //'admin'
+    ssl : true
 })
 
 module.exports = dbPool
