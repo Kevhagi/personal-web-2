48a350289ea50d0017979d640c71607da681e574
Kevhagi <94234971+Kevhagi@users.noreply.github.com>
2022-01-24 16:09:50 +0700

    Update db connection

--- connection/db.js
+++ connection/db.js
@@ -1,10 +1,10 @@
 const {Pool} = require('pg')
 
 const dbPool = new Pool({
-    database : 'personal_web',
+    database : 'd51cch6db1f81f', //'personal_web',
     port : 5432,
-    user : 'postgres',
-    password : 'admin'
+    user :  'tirtuvwurrnxfb', //'postgres',
+    password : '24be50d5b539e88cfd2618b0a6156e885ed479b0e658c3628d031a0b70e27c13' //'admin'
 })
 
 module.exports = dbPool
